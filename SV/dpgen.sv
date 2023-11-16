10bbbb75a6e9051c5ba0c45329c79a29fd48474a
CarstenLogan1 <carsten.logan@okstate.edu>
2023-11-16 17:28:43 -0600

    more work on stuff

--- SV/dpgen.sv
+++ SV/dpgen.sv
@@ -10,3 +10,40 @@ flopenrc flop (clk, reset, clear, en, d, w1);
 datapath dp(w1, w2);
 
 endmodule
+
+
+module FSM (clk, reset, start, gridin, gridout, en)
+input logic clk, reset, start;
+input logic [63:0] gridin;
+output logic [63:0] gridout;
+output logic en;
+
+
+typedef enum 	logic [1:0] {S0, S1, S2, S3} statetype;
+   statetype state, nextstate;
+
+  always_ff @(posedge clk, posedge reset)
+     if (reset) state = S0;
+     else       state = nextstate;
+   
+   // next state logic
+   always_comb
+     case (state)
+       S0: begin
+        if (start) nextstate = S1;
+    else nextstate = S0;
+       end
+       S1: begin  	
+	  if (start) nextstate = S1;
+      else nexstate = S2;
+       end
+       S2: begin  
+
+	  if (start) nextstate = S1;
+      else nexstate = S2;
+       end
+       endcase
+
+
+
+endmodule
--- SV/flopenrc.sv
+++ SV/flopenrc.sv
@@ -24,7 +24,7 @@
 // and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-module flopenrc #(parameter WIDTH = 8) (
+module flopenrc #(parameter WIDTH = 64) (
   input  logic             clk, reset, clear, en,
   input  logic [WIDTH-1:0] d, 
   output logic [WIDTH-1:0] q);
