ab6e2fbf3c5751156cb0fb764bf905e37832b3ea
Gabcorn <144495299+gabcorn@users.noreply.github.com>
2023-11-17 15:31:19 -0600

    fsm consolodated to dpgen

--- SV/dpgen.sv
+++ SV/dpgen.sv
@@ -1,52 +1,58 @@
-module dpgen(input logic [63:0] gridin, input logic reset, enable, clear, clk, output logic [63:0] gridout
+module dpgen(
+    input logic [63:0] gin, seed
+    input logic reset, clear, clk, start,
+    output logic [63:0] gout
 );
 
-logic [63:0] w1, w2;
+logic en;
+//logic [63:0] w1, w2;
 
-flopenrc flop (clk, reset, clear, en, w1, w2);
-datapath dp(w2, gridout);
+flopenrc flop (clk, reset, clear, en, gout, gin);
+datapath dp(w1, w2);
 
-endmodule
-
-
-module FSM (clk, reset, start, gridin, gridout, en)
-input logic clk, reset, start;
-input logic [63:0] gridin;
-output logic [63:0] gridout;
-output logic en;
-logic clear;
-assign clear = 0;
-
-dpgen gen(gin, reset, en, clear, clk, gout);
-
-
-typedef enum 	logic [1:0] {S0, S1, S2, S3} statetype;
+typedef enum 	logic [3:0] {S0, S1, S2, S3} statetype;
    statetype state, nextstate;
 
-  always_ff @(posedge clk, posedge reset)
-     if (reset) state = S0;
-     else       state = nextstate;
-   
-   // next state logic
-   always_comb
+ always_ff @(posedge clk, posedge reset)
+     if (reset) state <= S0;
+     else       state <= nextstate;
+
+
+always_comb
      case (state)
        S0: begin
-        en = start;
-        if (start) nextstate = S1;
-        else nextstate = S0;
+	     gout = seed;
+          en = 1;
+          //once we hit posedge clock the flipflop will send the seed to the datapath then gin will be updated after ??? nanoseconds.
+          if(start) 
+               nextstate <= S1;
+          else
+               nextstate <= S0;
        end
-       S1: begin 
-         	
-	  if (start) nextstate = S1;
-      else nexstate = S2;
+       S1: begin
+          gout = gin;
+          en = 1;
+          if(start)
+               nextstate <= S1;
+          else
+               nextstate <= S2;
        end
-       S2: begin  
-
-	  if (start) nextstate = S1;
-      else nexstate = S2;
+       S2: begin
+          en = 0;
+          if(start)
+               nextstate <= S1;
+          else
+               nextstate <= S2;
        end
-       endcase
-
-
+     endcase
+ 
 
 endmodule
+
+
+module FSM(
+
+
+
+
+) endmodule;
